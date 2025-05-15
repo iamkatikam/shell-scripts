8003ce857679ccd305b1795eb93c0cf0dd9916da
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-15 19:57:33 +0530

    hello world

---
+++ 01-hello-world.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello, World!"
+echo "This is a simple shell script."
+echo "It prints a message to the console."
+echo "You can run this script by executing: ./01-hello-world.sh"
+echo "Make sure to give it execute permissions first using: chmod +x 01-hello-world.sh"
+echo "Enjoy scripting!"
+echo "Goodbye!"
+echo "This is the end of the script."
+
