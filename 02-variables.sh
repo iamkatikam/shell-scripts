e93601530328417bb92e37966ba67990c56eb2eb
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-17 19:06:29 +0530

    condition checks

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,18 +5,4 @@ NUMBER2=20
 
 # Perform addition
 SUM=$((NUMBER1 + NUMBER2))
-# Perform subtraction
-DIFF=$((NUMBER1 - NUMBER2))
-# Perform multiplication
-PROD=$((NUMBER1 * NUMBER2))
-# Perform division
-if [ $NUMBER2 -ne 0 ]; then
-    DIV=$((NUMBER1 / NUMBER2))
-else
-    DIV="undefined (division by zero)"
-fi
-# Perform modulus
-MOD=$((NUMBER1 % NUMBER2))
-# Perform exponentiation
-EXP=$((NUMBER1 ** 2))
-
+echo "Sum: $SUM"
