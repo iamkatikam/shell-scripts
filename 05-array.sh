b88ed1d49ec2692d8e83f131f4f5323fc5432228
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-18 18:15:53 +0530

    read input

---
+++ 05-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+array=(1 2 3 4 5)
+echo "Array elements: ${array[@]}"
+echo "First element: ${array[0]}"
+echo "Second element: ${array[1]}"
+echo "Third element: ${array[2]}"
+echo "Fourth element: ${array[3]}"
+echo "Fifth element: ${array[4]}"
+echo "Number of elements in array: ${#array[@]}"
+
