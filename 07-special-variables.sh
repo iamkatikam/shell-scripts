181562f0efefd8f607f382695f95a755413cbf29
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-18 21:37:34 +0530

    read input

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -7,7 +7,7 @@ echo "The PID of the script is: $$"
 echo "The present working directory is: $PWD"
 echo "The home directory of the user is: $HOME"
 echo "The script execution user is: $USER"
-sleep 10
+sleep 10 &
 echo "PID of most recently backgrounded process is: $!"
 echo "The last command executed was: $?"
 echo "All the arguments passed to the script are: $*"
