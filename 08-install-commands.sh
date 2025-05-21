c9e34512045f6cfaf78bc0d7d46e61da853e66f1
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-21 22:14:53 +0530

    mysql install commands

---
+++ 08-install-commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will install the commands in the system
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+    echo "MySQL is not installed. Installing MySQL..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]; then
+        echo "MySQL installed successfully."
+    else
+        echo "Failed to install MySQL."
+        exit 1
+    fi
+else
+    echo "MySQL is already installed."
+fi
