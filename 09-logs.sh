02442b8ecad9d186edcdbcd8b13e3ca60316c6f1
Ramesh Katikam <katikam.rams@gmail.com>
2025-05-21 23:16:39 +0530

    mysql install commands

---
+++ 09-logs.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_DIR="/var/logs/shell-scripts"
+SCRIPT_NAME=$(basename $0)
+LOG_FILE="$LOGS_DIR/$SCRIPT_NAME.log"
+
+if [ ! -d $LOGS_DIR ]; then
+    mkdir $LOGS_DIR
+    echo -e "$G Created the logs directory. $N"
+else
+    echo -e "$Y Logs directory already exists. $N"
+fi
+
+userid=$(id -u)
+if [ $userid -ne 0 ]; then
+    echo -e "$R You need to run this script as root or with sudo. $N"
+    exit 1
+    else
+    echo "You are running this script as root." 
+fi
+
+VALIDATE()
+{
+    if [ $? -ne 0 ]; then
+        echo -e "$G Installing $2 is Successful. $N"
+        else
+        echo -e "$R Installing $2 is not Successful. $N"
+        exit 1
+    fi
+}
+# This script will install the mysql in the system
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+    echo -e "MySQL is not installed. Installing MySQL..."
+    dnf install mysql -y
+    VALIDATE $? MySQL   
+else
+    echo -e "$Y MySQL is already installed. $N"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    echo "Python3 is not installed. Installing Python3..."
+    dnf install python3 -y
+    VALIDATE $? Python3
+else
+    echo -e "$Y Python3 is already installed. $N"
+fi
+
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    echo "Nginx is not installed. Installing Nginx..."
+    dnf install nginx -y
+    VALIDATE $? Nginx 
+    else
+    echo -e "$Y Nginx is already installed. $N"
+fi
